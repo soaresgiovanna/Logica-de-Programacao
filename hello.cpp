4d9d8a9e2e08c186544519e753faa6684e18b286
Giovanna Soares <162447449+soaresgiovanna@users.noreply.github.com>
2024-05-24 12:38:29 -0300

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main ()
+{
+    int num1, num2, soma;
+
+    cout << "Soma de dois numeros";
+
+    cout << "Entre com o primeiro numero: " ;
+    cin >> num1;
+
+    cout << "Entre com o segundo numero: ";
+    cin >> num2;
+
+    soma = num1 + num2;
+
+    cout << "A soma e: ";
+    cout << soma;
+
+    system ("pause > 0");
+
+    return 0;
+}
