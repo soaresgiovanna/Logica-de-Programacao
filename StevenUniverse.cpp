6957fec2fd2adc22324aa88726e937f36f359992
Giovanna Soares <162447449+soaresgiovanna@users.noreply.github.com>
2024-06-12 11:29:19 -0300

    Add files via upload

---
+++ StevenUniverse.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream> //fluxo de arquivos, leitura e escrita
+#include <cstring>
+using namespace std;
+
+
+
+
+
+
+
+
+
+int main()
+{
+  cout << "Steven Universo é um  desenho animado voltado para o público jovem, e aborda muitas questão sociais e políticas[...]";
+
+
+
+}
